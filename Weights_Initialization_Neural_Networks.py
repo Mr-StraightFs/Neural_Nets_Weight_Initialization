ddef840a23e116766f2e15483ee7b95f5aa4e8b6
Mr-StraightFs <amin.lh.mba@gmail.com>
2021-06-04 16:10:37 +0100

    data vizualization

---
+++ Weights_Initialization_Neural_Networks.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn
+import sklearn.datasets
+from public_tests import *
+from init_utils import sigmoid, relu, compute_loss, forward_propagation, backward_propagation
+from init_utils import update_parameters, predict, load_dataset, plot_decision_boundary, predict_dec
+
+#Data Vizualization in 2D plot
+plt.rcParams['figure.figsize'] = (7.0, 4.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
