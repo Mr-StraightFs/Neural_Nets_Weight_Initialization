b5ff5c3c3fc5b78d23571e7bc0928bbefc745add
Mr-StraightFs <amin.lh.mba@gmail.com>
2021-06-05 16:11:35 +0100

    Initialization with Random and He Methods

--- Weights_Initialization_Neural_Networks.py
+++ Weights_Initialization_Neural_Networks.py
@@ -16,7 +16,7 @@ train_X, train_Y, test_X, test_Y = load_dataset()
 
 
 # Model Def
-def model(X, Y, learning_rate=0.01, num_iterations=15000, print_cost=True, initialization="he"):
+def model(X, Y, learning_rate=0.01, num_iterations=1000, print_cost=True, initialization="he"):
 
     grads = {}
     costs = []  # to keep track of the loss
@@ -73,3 +73,97 @@ def initialize_parameters_zeros(layers_dims):
          parameters['b' + str(l)] = np.zeros((layers_dims[l], 1)) #b1 -- bias vector of shape (layers_dims[1], 1)
 return parameters
 
+# Testing the Function and training the Model
+parameters = initialize_parameters_zeros([3, 2, 1])
+print("W1 = " + str(parameters["W1"]))
+print("b1 = " + str(parameters["b1"]))
+print("W2 = " + str(parameters["W2"]))
+print("b2 = " + str(parameters["b2"]))
+initialize_parameters_zeros_test(initialize_parameters_zeros)
+# Training on 1000 iterations
+parameters = model(train_X, train_Y, initialization = "zeros")
+print ("On the train set:")
+predictions_train = predict(train_X, train_Y, parameters)
+print ("On the test set:")
+predictions_test = predict(test_X, test_Y, parameters)
+print ("predictions_train = " + str(predictions_train))
+print ("predictions_test = " + str(predictions_test))
+plt.title("Model with Zeros initialization")
+axes = plt.gca()
+axes.set_xlim([-1.5,1.5])
+axes.set_ylim([-1.5,1.5])
+plot_decision_boundary(lambda x: predict_dec(parameters, x.T), train_X, train_Y)
+# The performance is bad , the cost doesn't decrease, and the algorithm performs no better than random guessing
+# Conclusion : initializing all the weights to zero results in the network failing to break symmetry.
+# The weights  𝑊[𝑙]  should be initialized randomly to break symmetry.
+# However, it's okay to initialize the biases  𝑏[𝑙]  to zeros. Symmetry is still broken so long as  𝑊[𝑙]  is initialized randomly.
+
+
+# The code for the Random Initialize Func.
+def initialize_parameters_random(layers_dims):
+    parameters = {}
+    L = len(layers_dims)  # integer representing the number of layers
+
+    for l in range(1, L):
+        parameters['W' + str(l)] = np.random.randn(layers_dims[l], layers_dims[l - 1]) * 10 #WL -- weight matrix of shape (layers_dims[L], layers_dims[L-1])
+        parameters['b' + str(l)] = np.zeros((layers_dims[l], 1)) * 10 #bL -- bias vector of shape (layers_dims[L], 1)
+return parameters
+
+# Testing and training with random initialization
+parameters = initialize_parameters_random([3, 2, 1])
+print("W1 = " + str(parameters["W1"]))
+print("b1 = " + str(parameters["b1"]))
+print("W2 = " + str(parameters["W2"]))
+print("b2 = " + str(parameters["b2"]))
+initialize_parameters_random_test(initialize_parameters_random)
+parameters = model(train_X, train_Y, initialization = "random")
+print ("On the train set:")
+predictions_train = predict(train_X, train_Y, parameters)
+print ("On the test set:")
+predictions_test = predict(test_X, test_Y, parameters)
+print (predictions_train)
+print (predictions_test)
+plt.title("Model with large random initialization")
+axes = plt.gca()
+axes.set_xlim([-1.5,1.5])
+axes.set_ylim([-1.5,1.5])
+plot_decision_boundary(lambda x: predict_dec(parameters, x.T), train_X, train_Y)
+# the results are much better than Zero initialization :
+# the cost learning curve seems to be systemteclly decresing wth the num of iterations . That's wot we desire .
+# the cost starts very high. This is because with large random-valued weights . The next initialization methods will solve this .
+# Poor initialization can lead to vanishing/exploding gradients, which also slows down the optimization algorithm.
+# Initializing weights to very large random values doesn't work well.
+# Initializing with small random values should do better. The important question is, how small should be these random values be?
+
+#
+# the Func. Code for the He Initialization Method
+def initialize_parameters_he(layers_dims):
+    np.random.seed(3)
+    parameters = {}
+    L = len(layers_dims) - 1  # integer representing the number of layers
+
+    for l in range(1, L + 1):
+        parameters['W' + str(l)] = np.random.randn(layers_dims[l], layers_dims[l - 1]) * (
+                    (2 / layers_dims[l - 1]) ** (1 / 2))
+        parameters['b' + str(l)] = np.zeros((layers_dims[l], 1)) * ((2 / layers_dims[l - 1]) ** (1 / 2))
+
+return parameters
+# Testing and Training with the He Method
+parameters = initialize_parameters_he([2, 4, 1])
+print("W1 = " + str(parameters["W1"]))
+print("b1 = " + str(parameters["b1"]))
+print("W2 = " + str(parameters["W2"]))
+print("b2 = " + str(parameters["b2"]))
+initialize_parameters_he_test(initialize_parameters_he)
+# parameters
+parameters = model(train_X, train_Y, initialization = "he")
+print ("On the train set:")
+predictions_train = predict(train_X, train_Y, parameters)
+print ("On the test set:")
+predictions_test = predict(test_X, test_Y, parameters)
+# plotting and Vizualization
+plt.title("Model with He initialization")
+axes = plt.gca()
+axes.set_xlim([-1.5,1.5])
+axes.set_ylim([-1.5,1.5])
+plot_decision_boundary(lambda x: predict_dec(parameters, x.T), train_X, train_Y)
